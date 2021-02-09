40736019198b2c486e8582f60c61a6762dc2d754
Serkan <serkara2de@hotmail.com>
2021-02-09 21:13:01 +0100

    Some practice added

--- src/test/java/group17_Members/Serkan.java
+++ src/test/java/group17_Members/Serkan.java
@@ -3,6 +3,8 @@ package group17_Members;
 public class Serkan {
     public static void main(String[] args) {
         System.out.println("Hallo This from Serkan");
+
+        System.out.println("Some practice");
     }
 
 }
